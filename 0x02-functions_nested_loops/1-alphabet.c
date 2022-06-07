bdf62276a441d1bd7bba669ec76291cb1630590b
Adeyemo Oluwademilade <teamergency112@gmail.com>
2022-06-07 15:14:28 -0700

    committedd

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabets
+ *
+ * return value is 0
+ */
+void print_alphabet(void)
+{
+	char alphabet = 'a';
+	while (alphabet <= 'z')
+	{
+		_putchar(alphabet);
+		alphabet++;
+	}
+	_putchar('\n');
+}
