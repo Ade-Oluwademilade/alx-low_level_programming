c40df9b318c17c2842007b6fb289d4aa124ef7f9
Adeyemo Oluwademilade <teamergency112@gmail.com>
2022-06-07 15:15:56 -0700

    added

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,18 @@
 #include "main.h"
 /**
- * print_alphabets
+ * print_alphabet - entry
  *
  * return value is 0
  */
 void print_alphabet(void)
 {
+
 	char alphabet = 'a';
 	while (alphabet <= 'z')
 	{
 		_putchar(alphabet);
 		alphabet++;
+
 	}
 	_putchar('\n');
 }
