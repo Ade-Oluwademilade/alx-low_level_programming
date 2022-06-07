77a407b0edadf8f63de75971eca9aab1848a491d
Adeyemo Oluwademilade <teamergency112@gmail.com>
2022-06-07 15:36:52 -0700

    3_islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _islower - Short description
+ * @c: contains value that will be compared
+ *
+ * Return 1 if true and 0 otherwise
+ */
+int _islower(int c)
+{
+	if (c > 'a' && c < 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
