8743237ceaa2c825ab65df476622adb53bc1dbc4
Adeyemo Oluwademilade <teamergency112@gmail.com>
2022-06-08 13:37:25 -0700

    file

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
- * main - Tests function that prints integer
+ * main - Tests function that prints integeri is positive or negative
  * Return: 0
  */
 int main(void)
 {
-	int a;
+	int i;
 
-	a = 0;
-	positive_or_negative(a);
+	i = 0;
+	positive_or_negative(i);
 
 	return (0);
 }
