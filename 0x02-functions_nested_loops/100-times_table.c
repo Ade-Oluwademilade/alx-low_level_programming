f6784d23f4870a6d82cb5a61ea1503aea881b9ad
Adeyemo Oluwademilade <teamergency112@gmail.com>
2022-06-08 13:07:47 -0700

    fix

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,9 +9,7 @@ void print_times_table(int n)
 	int a = 0, rep, b;
 
 	if (n < 0 || n > 15)
-	{
 		return;
-	}
 
 	while (a <= n)
 	{
@@ -19,9 +17,7 @@ void print_times_table(int n)
 		{
 			rep = a * b;
 			if (b == 0)
-			{
 				_putchar('0' + rep);
-			}
 			else if (rep < 10)
 			{
 				_putchar(' ');
