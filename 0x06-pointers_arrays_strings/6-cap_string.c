9d059c2b3b088972b3c36fafb56bfeec62c09acb
Adeyemo Oluwademilade <teamergency112@gmail.com>
2022-06-15 14:01:08 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,8 +11,8 @@ char *cap_string(char *s)
 	int separators[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
 	if (*(s + count) >= 97 && *(s + count) <= 122)
-		*(s + count) = *(s + count) - 32;
-		count++
+	*(s + count) = *(s + count) - 32;
+	count++
 	while (*(s + count) != '\0')
 	{
 		for (i = 0; i < 13; i++)
@@ -21,11 +21,11 @@ char *cap_string(char *s)
 			{
 
 				if ((*(s + (count + 1)) >= 97) && (*(s + (count + 1)) <= 122))
-					*(s + (count + 1)) = *(s + (count + 1)) - 32;
-					break;
+				*(s + (count + 1)) = *(s + (count + 1)) - 32;
+				break;
 			}
 		}
-			count++
+		count++
 	}
 	return	(s);
 }
